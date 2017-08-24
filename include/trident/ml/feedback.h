3566a75d843590f796bd7e58e387b520c444af8d
Jacopo Urbani <jacopo@cs.vu.nl>
2017-08-24 10:23:30 +0200

    forgot to add some new files in the previous commit

---
+++ include/trident/ml/feedback.h
@@ -0,0 +1,25 @@
+#ifndef _FEEDBACK_H
+#define _FEEDBACK_H
+
+#include <trident/ml/tester.h>
+
+#include <unordered_map>
+
+class Feedback {
+    private:
+        std::unordered_map<uint64_t, std::vector<uint32_t>> queries_po;
+        std::unordered_map<uint64_t, std::vector<uint32_t>> queries_sp;
+        uint16_t currentEpoch = 0;
+        uint64_t excluded;
+
+    public:
+        bool shouldBeIncluded(long s, long p, long o);
+
+        void setCurrentEpoch(uint16_t epoch) {
+            currentEpoch = epoch;
+        }
+
+        void addFeedbacks(std::shared_ptr<Tester<double>::OutputTest>);
+};
+
+#endif
---
+++ src/trident/ml/feedback.cpp
@@ -0,0 +1,64 @@
+#include <trident/ml/feedback.h>
+
+#include <boost/log/trivial.hpp>
+
+bool Feedback::shouldBeIncluded(long s, long p, long o) {
+    if (currentEpoch < 20) {
+        return true;
+    }
+    uint64_t key_spo = (uint64_t)s << 32;
+    key_spo |= p;
+    if (queries_sp.count(key_spo)) {
+        auto el = queries_sp.find(key_spo);
+        long positions = 0;
+        for (auto pos : el->second) {
+            positions += pos;
+        }
+        double avgpos = positions / el->second.size();
+        if (avgpos < 10) {
+            //BOOST_LOG_TRIVIAL(debug) << "Skipping (SP) " << s << " " << p << " " << o << " avg=" << avgpos << " positions=" << positions << " npos=" << el->second.size();
+            excluded++;
+            return false;
+        }
+    }
+    uint64_t key_pos = (uint64_t)p << 32;
+    key_pos |= o;
+    if (queries_po.count(key_pos)) {
+        //BOOST_LOG_TRIVIAL(debug) << "Skipping (PO) " << s << " " << p << " " << o;
+        auto el = queries_po.find(key_pos);
+        long positions = 0;
+        for (auto pos : el->second) {
+            positions += pos;
+        }
+        double avgpos = positions / el->second.size();
+        if (avgpos < 10) {
+            //BOOST_LOG_TRIVIAL(debug) << "Skipping (SP) " << s << " " << p << " " << o << " avg=" << avgpos << " positions=" << positions << " npos=" << el->second.size();
+            excluded++;
+            return false;
+        }
+    }
+    return true;
+}
+
+void Feedback::addFeedbacks(std::shared_ptr<Tester<double>::OutputTest> out) {
+    BOOST_LOG_TRIVIAL(debug) << "Adding feedbacks ...";
+    BOOST_LOG_TRIVIAL(debug) << "Excluded triples so far: " << excluded;
+    excluded = 0;
+    queries_sp.clear();
+    queries_po.clear();
+    for (auto q : out->results) {
+        uint64_t key_sp = (uint64_t)q.s << 32;
+        key_sp |= (uint64_t)q.p;
+        uint64_t key_po = (uint64_t)q.p << 32;
+        key_po |= (uint64_t)q.o;
+        if (!queries_sp.count(key_sp)) {
+            queries_sp.insert(std::make_pair(key_sp, std::vector<uint32_t>()));
+        }
+        queries_sp[key_sp].push_back(q.posO);
+        if (!queries_po.count(key_po)) {
+            queries_po.insert(std::make_pair(key_po, std::vector<uint32_t>()));
+        }
+        queries_po[key_po].push_back(q.posS);
+    }
+    BOOST_LOG_TRIVIAL(debug) << "done.";
+}
