b1eec41fb428ba2f91b21b1f6b0da0afab5b375b
Jacopo Urbani <jacopo@cs.vu.nl>
2017-12-14 16:58:51 +0100

    moved some code to allow a more modular execution of different algorithms

--- include/trident/ml/distmultester.h
+++ include/trident/ml/distmultester.h
@@ -13,20 +13,20 @@ class DistMulTester : public Tester<K> {
         double closeness(K *v1, K *v2, uint16_t dim) {
             double res = 0;
             for(uint16_t i = 0; i < dim; ++i) {
-                res += abs(v1[i] - v2[i]);
+                res += v1[i] * v2[i];
             }
             return res;
         }
 
         void predictO(K *s, uint16_t dims, K *p, uint16_t dimp, K* o) {
             for (uint16_t i = 0; i < dims; ++i) {
-                o[i] = s[i] + p[i];
+                o[i] = s[i] * p[i];
             }
         }
 
         void predictS(K *s, K *p, uint16_t dimp, K* o, uint16_t dimo) {
             for (uint16_t i = 0; i < dimo; ++i) {
-                s[i] = o[i] - p[i];
+                s[i] = o[i] * p[i];
             }
         }
 };
