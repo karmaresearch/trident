e4cd86597ebee2c7edf4b7d5943bdd2fe477cd36
Jacopo Urbani <jacopo@cs.vu.nl>
2018-03-09 08:19:18 +0100

    bugfixes

--- include/trident/utils/memoryfile.h
+++ include/trident/utils/memoryfile.h
@@ -35,11 +35,23 @@ class MemoryMappedFile {
         MemoryMappedFile(std::string file, bool ro, off_t start,
                 size_t len) {
 #if defined(_WIN32)
-			fd = CreateFile(file.c_str(), GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+			fd = CreateFile(file.c_str(), ro ? GENERIC_READ : GENERIC_READ | GENERIC_WRITE, FILE_SHARE_READ, NULL, OPEN_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
 			LARGE_INTEGER size;
 			GetFileSizeEx(fd, &size);
-			mappedfd = CreateFileMapping(fd, NULL, PAGE_READONLY, 0, 0, file.c_str());
-			data = (char *)MapViewOfFileEx(mappedfd, FILE_MAP_READ, HIWORD(start), LOWORD(start), len, NULL);
+			mappedfd = CreateFileMapping(fd, NULL, ro ? PAGE_READONLY : PAGE_READWRITE, 0, 0, NULL);
+			if (!mappedfd) {
+				std::string error;
+				DWORD errorMessageID = ::GetLastError();
+				if (errorMessageID != 0) {
+					LPSTR messageBuffer = nullptr;
+					size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
+						NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
+					error = string(messageBuffer, size);
+					LocalFree(messageBuffer);
+				}
+				LOG(ERRORL) << "Error mapping file " << file << " Error: " << error;
+			}
+			data = (char *)MapViewOfFileEx(mappedfd, ro ? FILE_MAP_READ : FILE_MAP_ALL_ACCESS, HIWORD(start), LOWORD(start), len, NULL);
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
             fd = ::open(file.c_str(), ro ?  O_RDONLY : (O_RDWR | O_CREAT),
 		    ro ? S_IREAD : (S_IREAD | S_IWRITE));
@@ -84,10 +96,11 @@ class MemoryMappedFile {
             return length;
         }
 
-        void flushAll() {
+		void flushAll() {
 #if defined(_WIN32)
-			LOG(ERRORL) << "flushAll memory mapped file is not implemented";
-			throw 10;
+			if (!FlushViewOfFile(data, length)) {
+				LOG(ERRORL) << "Flushing file returned an error!";
+			}
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
             msync(data, length, MS_SYNC);
 #endif
@@ -104,8 +117,9 @@ class MemoryMappedFile {
 
         void flush(off_t begin, size_t len) {
 #if defined(_WIN32)
-			LOG(ERRORL) << "flush memory mapped file is not implemented";
-			throw 10;
+			if (!FlushViewOfFile(data + begin, len)) {
+				LOG(ERRORL) << "Flushing file returned an error!";
+			}
 #elif defined(__unix__) || defined(__unix) || defined(unix) || (defined(__APPLE__) && defined(__MACH__))
             msync(data + begin, len, MS_SYNC);
 #endif
