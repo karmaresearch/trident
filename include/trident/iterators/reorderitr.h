21a6dec430b997dc592155247e54faa9cd2d9341
Ceriel Jacobs <c.j.h.jacobs@vu.nl>
2021-03-22 09:08:42 +0100

    Fix

--- include/trident/iterators/reorderitr.h
+++ include/trident/iterators/reorderitr.h
@@ -135,6 +135,9 @@ public:
     }
 
 	LIBEXP void moveto(const int64_t c1, const int64_t c2) {
+        if (itr == NULL || ! itr->hasNext()) {
+            next();
+        }
         itr->moveto(c1, c2);
     }
 
